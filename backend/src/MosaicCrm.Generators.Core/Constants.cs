a8816b246105622a362d796ab681a1f69cdcffd6
Антон Аврамчук <avramchuk.a@promail366.com>
2025-10-01 17:14:03 +0300

    add nullability. add constants

---
+++ backend/src/MosaicCrm.Generators.Core/Constants.cs
@@ -0,0 +1,19 @@
+﻿using MosaicCRM.Core;
+using MosaicCRM.Core.DependencyInjection;
+using MosaicCRM.Core.Modularity;
+using MosaicCRM.Core.Modularity.Abstractions;
+
+namespace MosaicCrm.Generators.Core
+{
+    public static class Constants
+    {
+        public static string? BoostrapperAttributeName = typeof(BootstrapperAttribute).FullName;
+
+        public static string? ModuleTypeName = typeof(ICrmModule).FullName;
+
+        public static string? ExportAttributeName = typeof(ExportAttribute).FullName;
+
+
+        public static string? DependsOnAttributeName = typeof(DependsOnAttribute).FullName;
+    }
+}
