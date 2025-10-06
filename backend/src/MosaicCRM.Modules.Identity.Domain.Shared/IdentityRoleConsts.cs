59770673fe4d8ec6628fedf8196f08f82b22aa64
Антон Аврамчук <avramchuk.a@promail366.com>
2025-10-06 06:47:35 +0300

    add identity shared

---
+++ backend/src/MosaicCRM.Modules.Identity.Domain.Shared/IdentityRoleConsts.cs
@@ -0,0 +1,14 @@
+namespace MosaicCRM.Modules.Identity.Domain.Shared;
+
+public static class IdentityRoleConsts
+{
+    /// <summary>
+    /// Default value: 256
+    /// </summary>
+    public static int MaxNameLength { get; set; } = 256;
+
+    /// <summary>
+    /// Default value: 256
+    /// </summary>
+    public static int MaxNormalizedNameLength { get; set; } = 256;
+}
